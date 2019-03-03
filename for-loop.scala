1f527dee5c1982bf608780602d6f4884a3c1bd98
Serge Vartanov <vartanov.s@gmail.com>
2019-03-02 19:28:18 -0800

    For to and For until loop iteration

---
+++ for-loop.scala
@@ -0,0 +1,15 @@
+object ScalaTutorial {
+  def main(args: Array[String]){
+
+    var i = 0;
+    for (i <- 1 to 10) {
+      println(i);
+    }
+
+    val randLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (i <- 0 until randLetters.length) {
+      println(randLetters(i));
+      
+    }
+  }
+}
