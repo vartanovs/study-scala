88b9c75f9cda785339b277b80e1f2a60ece935ff
Serge Vartanov <vartanov.s@gmail.com>
2019-03-02 20:50:43 -0800

    Input Output Functionality

---
+++ io.scala
@@ -0,0 +1,16 @@
+object ScalaTutorial {
+  def main(args: Array[String]) {
+    val startNumber = 1;
+    val endNumber = 10;
+    val rnd = new scala.util.Random;
+    val randNumber = startNumber + rnd.nextInt( (endNumber - startNumber) + 1 );
+
+    var numberGuess = 0;
+    do {
+      print("Guess a number from " + startNumber + " to " + endNumber + " : ");
+      numberGuess = readLine.toInt;
+    } while (numberGuess != randNumber);
+
+    println("You guessed the secret number!");
+  }
+}
