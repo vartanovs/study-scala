1acc992cc32d9d940f27fea00f630c2001d17474
Serge Vartanov <vartanov.s@gmail.com>
2019-03-15 19:12:39 -0700

    Class and Class Inheritance

---
+++ class.scala
@@ -0,0 +1,69 @@
+object ScalaTutorial {
+  def main(args: Array[String]){
+    val rover = new Animal;
+    rover.setName("Rover");
+    rover.setSound("woof!");
+    printf("%s says %s\n", rover.getName, rover.getSound);
+
+    val whiskers = new Animal("Whiskers", "meow!");
+    println(s"${whiskers.getName} with id ${whiskers.id} says ${whiskers.getSound}");
+
+    val spike = new Dog("Spike", "Woof", "Grrr!");
+    spike.setName("Spikey");
+    println(s"${spike.getName} with id ${spike.id} says ${spike.getSound} and ${spike.getGrowl}");
+  }
+
+  class Animal(var name: String, var sound: String) {
+    this.setName(name)
+
+    var id = Animal.newIdNum;
+
+    def getName(): String = name;
+    def getSound(): String = sound;
+
+    def setName(name: String) {
+      if(!(name.matches(".*\\d+.*"))) {
+        this.name = name
+      } else {
+        this.name = "No Name";
+      }
+    }
+
+    def setSound(sound: String) {
+      this.sound = sound;
+    }
+
+    def this(name: String) {
+      this("No Name", "No Sound");
+      this.setName(name);
+    }
+
+    def this() {
+      this("No Name", "No Sound");
+    }
+  }
+
+  object Animal {
+    private var idNumber = 0;
+    private def newIdNum = {idNumber += 1; idNumber};
+  }
+
+  class Dog(name: String, sound: String, growl: String) extends Animal(name, sound) {
+    def getGrowl(): String = growl;
+
+    def this(name: String, sound: String) {
+      this("No Name", sound, "No Growl");
+      this.setName(name);
+    }
+
+    def this(name: String) {
+      this("No Name", "No Sound", "No Growl");
+      this.setName(name);
+    }
+
+    def this() {
+      this("No Name", "No Sound", "No Growl");
+    }
+
+  }
+}
